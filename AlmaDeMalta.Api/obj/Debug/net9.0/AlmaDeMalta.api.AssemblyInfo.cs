ffa88b7406e1341ddb42b4b750184dce491f00eb
fcampoa <felixcampoa@gmail.com>
2025-04-11 15:16:30 -0700

    adding gitignore

--- AlmaDeMalta.Api/obj/Debug/net9.0/AlmaDeMalta.api.AssemblyInfo.cs
+++ AlmaDeMalta.Api/obj/Debug/net9.0/AlmaDeMalta.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmaDeMalta.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6fbc6a1f68b54aa780b37cdba96abe75d63c00a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmaDeMalta.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmaDeMalta.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
