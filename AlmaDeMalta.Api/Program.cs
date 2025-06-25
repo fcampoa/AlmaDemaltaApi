9e4ec715b32a50a198945cf5ff4130f496629629
fcampoa <felixcampoa@gmail.com>
2025-06-24 17:04:58 -0700

    serilog fix

--- AlmaDeMalta.Api/Program.cs
+++ AlmaDeMalta.Api/Program.cs
@@ -14,7 +14,8 @@ builder.Host.UseSerilog((context, services, configuration) =>
     configuration
         .ReadFrom.Configuration(context.Configuration)
         .ReadFrom.Services(services)
-        .Enrich.FromLogContext();
+        .Enrich.FromLogContext()
+        .WriteTo.Console();
 });
 
 // Agregar servicios de FastEndpoints
